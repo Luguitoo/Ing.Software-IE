7ee10bcd56a0b4613cb757663e583700b53943f6
Carlos Lugo <lugostein201@gmail.com>
2023-05-25 12:47:34 -0300

    -Leer notas
    - En la ruta /notas ya se pueden leer todas las notas del alumno, su oportunidad, etc todo lo que viene del excel de sapientia del alumno.

--- application.py
+++ application.py
@@ -1,29 +1,35 @@
 from flask import Flask, url_for, redirect, render_template, send_file, request
 from config import DevConfig
 import sqlite3
-
+import os
 #Excel
 from openpyxl import load_workbook
 from openpyxl.styles import PatternFill
-
+import pandas as pd
+#Json
 from flask import jsonify
 
 application = app = Flask(__name__)
 
 app.config.from_object(DevConfig)
 dbtest = sqlite3.connect('NombreDeLaDB.db')
-
+##Vistas
 @app.route('/')
 def index():
     return render_template('index.html')
 
-##Ruta de descarga del modelo del excel
+@app.route('/notas')
+def notas():
+    return render_template('notas.html')
+##Procesos
+##Ruta de descarga del modelo del excel, terminado Lugo
 @app.route('/download_template', methods = ['GET', 'POST'])
 def download_template():
     if request.method == "POST":
         return send_file('./static/resources/IE-CyT.xlsx', as_attachment=True)
-
-@app.route('/read_excel', methods=['POST'])
+    
+##Leer excel de alumnos y carga en el front, terminado Lugo
+@app.route('/read_excel', methods=['POST']) 
 def read_excel():
     archivo = request.files['archivo']
     if "archivo" not in request.files:
@@ -61,6 +67,64 @@ def read_excel():
     json_data = jsonify(data)
     return json_data
 
+##Leer notas del excel
+@app.route('/read_notas', methods=['POST'])
+def read_notas():
+    if "archivo" not in request.files:
+        print("No se envió ningún archivo")
+        return "No se envió ningún archivo"
+    
+    archivo = request.files['archivo']
+    if archivo.filename == "":
+        print("No se seleccionó ningún archivo")
+        return "No se seleccionó ningún archivo"
+    
+    print(archivo.filename)
+   
+    # Cargamos el archivo
+    # Leer el archivo XLS con pandas
+    df = pd.read_excel(archivo)
+    # Guardar el archivo XLSX
+    archivo_xlsx = f"./static/resources/{archivo.filename}.xlsx"
+    df.to_excel(archivo_xlsx, index=False)
+    
+    wb = load_workbook('./static/resources/{a}.xlsx'.format(a = archivo.filename))
+    ws = wb["Sheet1"]
+    nomb = ws['C2'].value
+    matr = ws['C3'].value
+    print(f"Alumno: {nomb}, matricula: {matr}")
+    inicio = 4
+    cont = 0
+    b = True
+    data = []  # Lista para almacenar los datos
+
+    while b:
+        if not ws['A{a}'.format(a=str(inicio + 1))].value:
+            cont += 1
+            if cont > 1:
+                b = False
+        else:
+            mat = ws['A{a}'.format(a=str(inicio + 1))].value
+            cod = ws['D{a}'.format(a=str(inicio + 1))].value
+            opo = ws['E{a}'.format(a=str(inicio + 1))].value
+            nota = ws['F{a}'.format(a=str(inicio + 1))].value
+            act = ws['G{a}'.format(a=str(inicio + 1))].value
+            fec = ws['H{a}'.format(a=str(inicio + 1))].value
+            data.append(
+            {
+                'alu': ws['C2'].value,
+                'mat': mat,
+                'cod': cod,
+                'opo': opo,
+                'nota': nota,
+                'act': act,
+                'fec': fec
+            })
+        inicio += 1
+    # Convertir a JSON
+    json_data = jsonify(data)
+    os.remove('./static/resources/{a}.xlsx'.format(a = archivo.filename)) #elimina el excel del sistema
+    return json_data
 if __name__=='__main__':
     app.run(debug = True, port= 8000)
 
---
+++ static/js/notas.js
@@ -0,0 +1,44 @@
+const fileInput = document.getElementById('fileInput');
+const uploadButton = document.getElementById('uploadButton');
+
+uploadButton.addEventListener('click', () => {
+  const file = fileInput.files[0];
+
+  if (file) {
+    const formData = new FormData();
+    formData.append('archivo', file);
+
+    // Realizar la solicitud AJAX al backend
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/read_notas', true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        //status ok
+        const response = JSON.parse(xhr.responseText);
+        console.log(response); // Verificar los datos en la consola
+
+        //dom en tbody
+        const tableBody = document.querySelector("tbody");
+
+        for (let i = 0; i < response.length; i++) {
+          const rowData = response[i];
+          if (i == 1){
+            const tituloAlumno = document.querySelector('.title');
+            // Modificar alumno
+            tituloAlumno.innerHTML = `<i class="bi bi-file-person"></i> Alumno: ${rowData.alu}`;
+          }
+          const row = document.createElement("tr");
+          row.innerHTML = `<td>${rowData.mat}</td>
+                            <td>${rowData.cod}</td>
+                            <td>${rowData.opo}</td>
+                            <td>${rowData.nota}</td>
+                            <td>${rowData.act}</td>
+                            <td>${rowData.fec}</td> `;
+
+          tableBody.appendChild(row);
+        }
+      }
+    };
+    xhr.send(formData);
+  }
+});

002a551aedc41b5b3f4728a3931748ff951cb775
Fernandoodc <55900225+Fernandoodc@users.noreply.github.com>
2023-05-29 10:16:08 -0400

    Revert "- Ya carga alumnos y el historial"
    This reverts commit 13d136111acfe88b2aa127e7caca1b9df9fcbe92.

--- database/conexion.py
+++ database/conexion.py
@@ -2,12 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///dbTest.db"
+DATABASE_URL = "sqlite:///NombreDeLaDB.db"
 
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
-session = SessionLocal()
 
 Base = declarative_base()
 def get_db():

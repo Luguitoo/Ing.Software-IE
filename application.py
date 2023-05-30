387de6f2600f460b583aab2b1bd239ae4ae45ecc
Fernandoodc <55900225+Fernandoodc@users.noreply.github.com>
2023-05-30 14:51:58 -0400

    mejoras en detalle cohorte

--- application.py
+++ application.py
@@ -40,26 +40,40 @@ def index():
     cohortes = session.query(models.Cohortes).all()
     return render_template('index.html', data=cohortes)
 
+@app.route('/cohortes')
+def cohortes():
+    session = SessionLocal()
+    cohorte = session.query(models.Cohortes.cohorte_id).all()
+    if cohorte:
+        session.close()
+        return redirect('/selCoh?cid='+str(cohorte[0][0]))
+    else:
+        session.close()
+        return redirect('/')
+    
+
 @app.route('/selCoh', methods=['GET'])
 def selCoh():
+    session = SessionLocal()
     if request.method == 'GET':
         cid = request.args.get('cid')
-        print(cid)
         value = {'x': cid}
-        session = SessionLocal()
 
         con = text("select * from alumnos where alumnos.cohorte_id = :x")
         alumnos = session.execute(con, value)
-        return render_template('cohortes.html', data=alumnos, coh=value['x'])
+        cohortes = session.query(models.Cohortes).all()
+        cohorte = session.query(models.Cohortes).filter(models.Cohortes.cohorte_id==cid).first()
+        session = SessionLocal()
+        return render_template('cohortes.html', data=alumnos, coh=cohorte, cohortes = cohortes)
 
     #for i in range(1,8):
     #    print(tasa_promocion_semestral(1, i, session))
 
     """for i in outs:
         print(i.matricula, ' - ', i.nota)"""
-
+    cohortes = session.query(models.Cohortes).all()
     session.close()
-    return render_template('index.html')
+    return render_template('index.html', cohortes=cohortes)
 
 @app.route('/notas')
 def notas():
@@ -131,84 +145,88 @@ def read_excel():
     return json_data
 
 ##Leer notas del excel, terminado lugo
-@app.route('/read_notas', methods=['POST'])
+@app.route('/read_all_notas', methods=['POST'])
 def read_notas():
     session = SessionLocal()
+    archivos = request.files.getlist("archivo")
     if "archivo" not in request.files:
         print("No se envió ningún archivo")
         return "No se envió ningún archivo"
     
-    archivo = request.files['archivo']
-    if archivo.filename == "":
-        print("No se seleccionó ningún archivo")
-        return "No se seleccionó ningún archivo"
-    
-    print(archivo.filename)
-    # Cargamos el archivo
-    # Leer el archivo XLS con pandas
-    df = pd.read_excel(archivo)
-    # Guardar el archivo XLSX
-    archivo_xlsx = f"./static/resources/{archivo.filename}.xlsx"
-    df.to_excel(archivo_xlsx, index=False)
-    
-    wb = load_workbook('./static/resources/{a}.xlsx'.format(a = archivo.filename))
-    ws = wb["Sheet1"]
-    nomb = ws['C2'].value
-    matr = ws['C3'].value
-    matr = matr.split("/")[-1].strip() # esto devuelve la segunda matrícula
-    print(f"Alumno: {nomb}, matricula: {matr}")
-    #elimina de ls bd todos las calificaciones anteriores
-    #capaz update sea mejor
-    historial = session.query(models.Historial).filter(models.Historial.matricula == matr) #Se corrigió un error en el que no se eliminaba el historial viejo
-    if historial.count() > 0:
-        historial.delete()
-        session.commit()
-    inicio = 4
-    cont = 0
-    b = True
-    data = []  # Lista para almacenar los datos
+    #archivo = request.files['archivo']
+    for archivo in archivos:
+        print(archivo.filename)
+        if archivo.filename == "":
+            print("No se seleccionó ningún archivo")
+            return "No se seleccionó ningún archivo"
+        # Cargamos el archivo
+        # Leer el archivo XLS con pandas
+        df = pd.read_excel(archivo)
+        # Guardar el archivo XLSX
+        archivo_xlsx = f"./static/resources/{archivo.filename}.xlsx"
+        df.to_excel(archivo_xlsx, index=False)
+        
+        try:
+            wb = load_workbook('./static/resources/{a}.xlsx'.format(a = archivo.filename))
+            ws = wb["Sheet1"]
+            nomb = ws['C2'].value
+            matr = ws['C3'].value
+            matr = matr.split("/")[-1].strip() # esto devuelve la segunda matrícula
+            print(f"Alumno: {nomb}, matricula: {matr}")
+            #elimina de ls bd todos las calificaciones anteriores
+            #capaz update sea mejor
+            historial = session.query(models.Historial).filter(models.Historial.matricula == matr) #Se corrigió un error en el que no se eliminaba el historial viejo
+            if historial.count() > 0:
+                historial.delete()
+                session.commit()
+            inicio = 4
+            cont = 0
+            b = True
+            data = []  # Lista para almacenar los datos
 
-    while b:
-        if not ws['A{a}'.format(a=str(inicio + 1))].value:
-            cont += 1
-            if cont > 1:
-                b = False
-        else:
-            mat = ws['A{a}'.format(a=str(inicio + 1))].value
-            cod = ws['D{a}'.format(a=str(inicio + 1))].value
-            opo = ws['E{a}'.format(a=str(inicio + 1))].value
-            nota = ws['F{a}'.format(a=str(inicio + 1))].value
-            nota = nota.split(':')[0].strip()
-            act = ws['G{a}'.format(a=str(inicio + 1))].value
-            fec = ws['H{a}'.format(a=str(inicio + 1))].value
-            data.append(
-            {
-                'alu': ws['C2'].value,
-                'mat': mat,
-                'cod': cod,
-                'opo': opo,
-                'nota': nota,
-                'act': act,
-                'fec': fec
-            })
-            #carga la nueva calificación a bd
-            date = datetime.strptime(fec, '%d/%m/%Y')
-            newhistorial = models.Historial(matricula = matr, materia_codigo = cod, nota = nota, oportunidad = opo, fecha_examen = date)
-            session.add(newhistorial)
-            session.flush()
-        inicio += 1
-    # Convertir a JSON
-    json_data = jsonify(data)
-    os.remove('./static/resources/{a}.xlsx'.format(a = archivo.filename)) #elimina el excel del sistema
-    #print(json_data)
-    #coomit de la bd y cierre de sesión
-    id_cohorte = session.query(models.Alumnos.cohorte_id).filter(models.Alumnos.matricula==matr).first()
+            while b:
+                if not ws['A{a}'.format(a=str(inicio + 1))].value:
+                    cont += 1
+                    if cont > 1:
+                        b = False
+                else:
+                    mat = ws['A{a}'.format(a=str(inicio + 1))].value
+                    cod = ws['D{a}'.format(a=str(inicio + 1))].value
+                    opo = ws['E{a}'.format(a=str(inicio + 1))].value
+                    nota = ws['F{a}'.format(a=str(inicio + 1))].value
+                    nota = nota.split(':')[0].strip()
+                    act = ws['G{a}'.format(a=str(inicio + 1))].value
+                    fec = ws['H{a}'.format(a=str(inicio + 1))].value
+                    data.append(
+                    {
+                        'alu': ws['C2'].value,
+                        'mat': mat,
+                        'cod': cod,
+                        'opo': opo,
+                        'nota': nota,
+                        'act': act,
+                        'fec': fec
+                    })
+                    #carga la nueva calificación a bd
+                    date = datetime.strptime(fec, '%d/%m/%Y')
+                    newhistorial = models.Historial(matricula = matr, materia_codigo = cod, nota = nota, oportunidad = opo, fecha_examen = date)
+                    session.add(newhistorial)
+                    session.flush()
+                inicio += 1
+            # Convertir a JSON
+            json_data = jsonify(data)
+            os.remove('./static/resources/{a}.xlsx'.format(a = archivo.filename)) #elimina el excel del sistema
+            #print(json_data)
+            #coomit de la bd y cierre de sesión
+            id_cohorte = session.query(models.Alumnos.cohorte_id).filter(models.Alumnos.matricula==matr).first()
+        except:
+            os.remove('./static/resources/{a}.xlsx'.format(a = archivo.filename)) #elimina el excel del sistema
+            session.close()
+            return {'msg': 'Ocurrió un error a leer el documento'}
     session.commit()
     session.close()
     print(id_cohorte)
     return redirect('/selCoh?cid='+str(id_cohorte[0]))
-
-
 
 
 #Ver historial de asignaturas del alumno
@@ -223,8 +241,6 @@ def extraer_numero(string):
 def historial(mat:str):
     session = SessionLocal()
     semestres =  session.query(func.count(models.Semestre.semestre_id)).first()
-    #materias = session.execute(text(f"select historial.materia_codigo, historial.nota, materia.materia_descrip from historial, materias"))
-    #materias = session.query(models.Historial.materia_codigo ,models.Historial.nota, models.Materias.materia_descrip, ).outerjoin(models.Materias).filter(models.Historial.matricula==mat.upper()).order_by(models.Semestre.semestre_id).all()
     materias = session.query(models.Materias.materia_codigo, models.Materias.materia_descrip, models.Semestre.semestre_id).join(models.Semestre).order_by(models.Semestre.semestre_id).all()
     historial = []
     for materia in materias:
@@ -248,8 +264,9 @@ def historial(mat:str):
             else:
                 estado['estado'] = 'No aprovado'
         historial.append(estado)
+    alumno = session.query(models.Alumnos).filter(models.Alumnos.matricula == mat).first()
     session.close()
-    return render_template('historial.html', historial = historial)
+    return render_template('historial.html', historial = historial, alumno = alumno)
 
 
 if __name__=='__main__':
